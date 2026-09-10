f589a1a7d7c6ac2b6f8ce00a38dfeb90881787df
DanilShramov <99270727+DanilShramov@users.noreply.github.com>
2026-09-10 10:26:12 +0300

    Add files via upload

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,10 +1,10 @@
-// FLO Academy release 1.0062
-const RELEASE_VERSION='1.0062';
+// FLO Academy release 1.0063
+const RELEASE_VERSION='1.0063';
 
 export async function onRequest(context){
   const response=await context.next();
   const headers=new Headers(response.headers);
-  headers.set('X-FLO-Version', RELEASE_VERSION);
+  headers.set('X-FLO-Version',RELEASE_VERSION);
 
   const ct=response.headers.get('content-type')||'';
   if(!ct.includes('text/html')){
@@ -16,14 +16,15 @@ export async function onRequest(context){
   }
 
   let text=await response.text();
-  text=text.replace(/const APP_VERSION="[^"]+";/, `const APP_VERSION="${RELEASE_VERSION}";`);
+  text=text.replace(/const APP_VERSION="[^"]+";/,`const APP_VERSION="${RELEASE_VERSION}";`);
 
-  const scripts=`<script src="/release-version.js?v=${RELEASE_VERSION}"></script>
-<script src="/test-upgrade.js?v=${RELEASE_VERSION}"></script>
+  const scripts=`<script src="/test-upgrade.js?v=${RELEASE_VERSION}"></script>
 <script src="/results-filter.js?v=${RELEASE_VERSION}"></script>
+<script src="/overview-fix.js?v=${RELEASE_VERSION}"></script>
+<script src="/release-version.js?v=${RELEASE_VERSION}"></script>
 <script type="module">`;
 
-  const injected=text.replace('<script type="module">', scripts);
+  const injected=text.replace('<script type="module">',scripts);
   headers.delete('content-length');
 
   return new Response(injected,{
