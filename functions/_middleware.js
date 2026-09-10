2632b5fbf68dd2c6149898415281cd7c39ee86f9
DanilShramov <99270727+DanilShramov@users.noreply.github.com>
2026-09-10 19:34:46 +0300

    Add files via upload

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,7 +1,7 @@
-// FLO Academy 1.0075
+// FLO Academy 1.0076
 export async function onRequest(context){
  const response=await context.next();const headers=new Headers(response.headers);
- headers.set('X-FLO-Version','1.0075');
+ headers.set('X-FLO-Version','1.0076');
  if((headers.get('content-type')||'').includes('text/html'))headers.set('Cache-Control','no-cache, max-age=0, must-revalidate');
  return new Response(response.body,{status:response.status,statusText:response.statusText,headers});
 }
